431b23da62ce3b30c053d12b5c50c3e3aee369df
Ismo Puustinen <ismo.puustinen@intel.com>
2018-08-28 13:37:19 +0300

    cpu-pool-policy: add metrics type files.

---
+++ pkg/apis/cpupools.intel.com/v1alpha1/types.go
@@ -0,0 +1,30 @@
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+type Pool struct {
+	PoolName string `json:"poolName"`
+	Usage    int    `json:"usage"`
+}
+
+type MetricSpec struct {
+	Pools []Pool `json:"pools"`
+}
+
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type Metric struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec MetricSpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type MetricList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Metrics []Metric `json:"metrics"`
+}
